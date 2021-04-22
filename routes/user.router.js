2d7463ebc12827b826a27095ec117ad237686741
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-22 13:38:53 +0200

    Created user router with user register endpoint

---
+++ routes/user.router.js
@@ -0,0 +1,20 @@
+const router = require('express').Router({mergeParams:true});
+
+const userController = require("../controllers/user.controller");
+
+
+// USER ENDPOINTS
+
+
+// User Register
+
+router.post('/', async (req,res) => {
+  try{
+    res.json(await userController.createUser(req.body));
+  }catch(error){
+    res.status(500).json({
+      error: 'error',
+      message: 'The user could not be registered'
+    })
+  };
+});
