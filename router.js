528d84f7dbbb2714ef9add1bc2d241c775fe2e6a
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-22 13:44:36 +0200

    Setting the user path

--- router.js
+++ router.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
 
+const userRouter = require('./routes/user.router');
+
+// REST RESOURCES
+
+router.use('/users', userRouter);
+
 module.exports = router;
