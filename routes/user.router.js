a6500e7bb451c1e71c38d4925c6f6c31a98c7c10
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-22 16:00:56 +0200

    User login endpoint created

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router({mergeParams:true});
-
 const userController = require("../controllers/user.controller");
+const jwt = requier('jsonwebtoken');
 
 
 // USER ENDPOINTS
@@ -9,14 +9,30 @@ const userController = require("../controllers/user.controller");
 // User Register
 
 router.post('/', async (req,res) => {
-  try{
-    res.json(await userController.createUser(req.body));
-  }catch(error){
-    res.status(500).json({
-      error: 'error',
-      message: 'The user could not be registered'
-    })
+    try{
+      res.json(await userController.createUser(req.body));
+    }catch(error){
+      res.status(500).json({
+        error: 'error',
+        message: 'The user could not be registered'
+      })
   };
 });
+
+// User Login
+
+router.post('/login', async (req,res) => {
+    try {
+      const {email, password} = req.body;
+      const jwt = await userController.login(email, password);
+      res.json({jwt})
+    }catch(error){
+        res.status(500).json({
+          error: 'error',
+          message: 'The user could not be loged'
+        })
+  };
+})
+
 
 module.exports = router;
