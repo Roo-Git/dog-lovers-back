25b8b558439f957ea39a08e07c4ae872b60572a0
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-25 11:52:46 +0200

    Configured and installed cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const router = require ('./router');
 const db = require ('./db');
 const app = express();
 const port = 3000;
+const cors = require('cors')
 
 
 
@@ -15,6 +16,7 @@ app.get('/', function (req, res) {
 
 // Middleware
 app.use(express.json());
+app.use(cors())
 app.use(router)
 
 
