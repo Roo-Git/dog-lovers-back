f6b3fab73a357dae397bb350f373d0563247579b
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-27 11:20:11 +0200

    Deleted validate field 'model user'

--- models/user.js
+++ models/user.js
@@ -26,28 +26,9 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [10, 20],
         },
-      },
-
-      // Comentario de Robert de la Fuente 26/04/2021
-
-      // No he conseguido poner la contraseña que no sea nula.
-      
-      // Anteriormente Lo he intentado de las siguientes formas:
-      // con allowNull: false.
-      // con require: true.
-      // y en la validacion, poniendo notNull: true, o isNull: true.
-
-      // He consultado la siguiente documentación:
-      // https://sequelize.org/master/manual/validations-and-constraints.html
-
-      // He intentado darle un Len, pero no hay manera.
-      
-      
+      },   
       password: {       
         type: DataTypes.STRING,
-        validate: {
-          len: [5, 1000000],
-        },
       },
 
     firstName: DataTypes.STRING,
