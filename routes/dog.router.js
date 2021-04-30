5b6ad0b428260b9b514ad643a43c6d6b3f9d5cb3
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-30 11:00:44 +0200

    Added auth to create a dog by user Id

--- routes/dog.router.js
+++ routes/dog.router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router({mergeParams:true});
 const dogController = require("../controllers/dog.controller");
+const auth = require('../middlewares/auth');
 
-router.post ('/', async (req,res) => {
+router.post ('/:id' , auth, async (req,res) => {
   try{
     const user = req.params.user_Id
     res.json(await dogController.createDog(req.body, user,));
