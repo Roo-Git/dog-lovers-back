9c21dfbbb993653315376dfff74f7d0981f2af19
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-03 13:55:47 +0200

    Tryng to create a candidate if the sitter field that comes from the users table is true.

--- controllers/candidate.controller.js
+++ controllers/candidate.controller.js
@@ -6,6 +6,11 @@ class CandidateController {
 
   // Create Candidate
 
+  // Quiero crear una candidatura si se cumplen las siguiente condiciones:
+
+  // Si el campo sitter, que proviene de la tabla Users es TRUE.
+
+  // Si el campo sitter, es FALSE, tiene que dar error.
 
   async createCandidate (careRequest_Id, id) {
     return Candidate.create(careRequest_Id,{where:{id}});
