0d5745f0007b205b62abe0916bd3e4317dbfe6c3
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-03 16:11:15 +0200

    CRUD of candidate model finish.

--- controllers/candidate.controller.js
+++ controllers/candidate.controller.js
@@ -12,15 +12,40 @@ class CandidateController {
   // Si el campo sitter, es FALSE, tiene que dar error.
 
   async createCandidate (candidacy) {
-    let candidate = await User.findOne({where: {id: candidacy.sitter_Id}})
-    console.log(candidate)
+    let candidate = await User.findOne({where: {id: candidacy.sitter_Id}});
+
     if(candidate.sitter == true)
-    return await Candidate.create(candidacy);
+      return await Candidate.create(candidacy);
     else
-    return {
-      error: "Mensaje de Error"
-    }
-  } 
+      return {
+        error: "Mensaje de Error"
+      };
+  };
+  
+  // Get Candidate by ID
+
+  async findCandidate(id){
+    return Candidate.findOne({where:{id}})
+  }
+
+  // Index all Candidates
+
+  async indexAll(){
+    return Candidate.findAll()
+  };
+
+  // Update Candidate by ID 
+
+  async updateCandidate(update, id) {
+    const request = await Candidate.findOne({where: {id}})
+    return await request.update(update);
+  }; 
+  
+  // Delete Candidate by ID
+
+  async deleteCandidate(id) {
+    return Candidate.destroy({where:{id}})
+  };
 }
 
 let candidateController = new CandidateController();
--- routes/candidate.router.js
+++ routes/candidate.router.js
@@ -15,5 +15,64 @@ router.post('/', async (req,res) => {
   };
 });
 
+// Get Candidate by ID
+
+router.get ('/:id', async (req, res) => {
+  try {
+
+    const id = req.params.id;
+    const candidate = await candidateController.findCandidate(id);
+    const post = candidate.post
+    const confirmedBySitter = candidate.confirmedBySitter
+    const acceptedByOwner = candidate.acceptedByOwner
+    const sitter_Id = candidate.sitter_Id
+  
+    res.json({post, confirmedBySitter, acceptedByOwner, sitter_Id})
+
+  }catch (err) {
+    return res.status(500).json({
+      message: err.message
+    });
+  };
+});
+
+
+// Index All Candidates
+
+router.get ('/', async (req,res) => {
+  try{
+    res.json(await candidateController.indexAll());
+  }catch(error){
+    res.status(500).json({
+      error: 'error',
+      message: 'error'
+    });
+  };
+});
+
+// Update Candidate by ID
+
+router.put('/:id', async (req, res) => {
+  try{
+    const body = req.body;
+    res.json(await candidateController.updateCandidate(body, req.params.id));
+  }catch(err){
+    return res.status(500).json({
+      message: err.message
+    });
+  };
+});
+
+// Delete Candidate by ID
+
+router.delete('/:id', async (req, res) => {
+  try{
+    res.json(await candidateController.deleteCandidate(req.params.id));
+  }catch(err){
+    res.status(500).json({
+      message: err.message
+    });
+  };
+});
 
 module.exports = router;
