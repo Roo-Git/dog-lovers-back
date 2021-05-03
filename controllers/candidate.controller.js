9a43054696f5c6e850855373d729725f13d64cc4
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-03 14:37:57 +0200

    Refactored the function, problem solved

--- controllers/candidate.controller.js
+++ controllers/candidate.controller.js
@@ -1,20 +1,26 @@
 const {Candidate, User} = require ('../models')
-//const { Op } = require ("sequelize")
-//[Op.not]: true, ---> IS NOT TRUE
+
 
 class CandidateController {
 
   // Create Candidate
 
-  // Quiero crear una candidatura si se cumplen las siguiente condiciones:
+  // Quiero crear una candidato si se cumplen las siguiente condiciones:
 
   // Si el campo sitter, que proviene de la tabla Users es TRUE.
 
   // Si el campo sitter, es FALSE, tiene que dar error.
 
-  async createCandidate (careRequest_Id, id) {
-    return Candidate.create(careRequest_Id,{where:{id}});
-  }
+  async createCandidate (candidacy) {
+    let candidate = await User.findOne({where: {id: candidacy.sitter_Id}})
+    console.log(candidate)
+    if(candidate.sitter == true)
+    return await Candidate.create(candidacy);
+    else
+    return {
+      error: "Mensaje de Error"
+    }
+  } 
 }
 
 let candidateController = new CandidateController();
