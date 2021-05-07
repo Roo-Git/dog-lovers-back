39125c8ed5ff71625079d75d2a0509688a01ff5b
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-07 15:32:56 +0200

    Finish confirmation CRUD

--- controllers/confirmation.controller.js
+++ controllers/confirmation.controller.js
@@ -7,34 +7,39 @@ class ConfirmationController {
 
   async createConfirmation(confirmation) {
     let user_Id = confirmation.user_Id
-    console.log(user_Id, "USER ID")  
     let dog_Id = confirmation.dog_Id
-    console.log(user_Id, "DOG ID") 
     let candidate_Id = confirmation.candidate_Id
-    console.log(user_Id, "CANDIDATE ID") 
-
     let validateOwner = await Dog.findOne({where: {[Op.and]: [{ id: dog_Id }, { user_Id: user_Id }]}})
-    console.log(validateOwner)
     let validateCandidacy = await Candidate.findOne({where: {id: candidate_Id}})
-    console.log(validateCandidacy)
     if(validateCandidacy || validateOwner != null)
-    return await Confirmation.create(confirmation);
-    else
-    return {
-      error: "Mensaje de error"
-    }
+      return await Confirmation.create(confirmation);
+      else
+      return {
+        error: "Mensaje de error"
+      }
   };
 
   // Get Confirmation by ID
-
+  async findConfirmation(id){
+    return Confirmation.findOne({where:{id}})
+  };
 
   // Get All Confirmations
-
+  async indexAll(){
+    return Confirmation.findAll()
+  };
 
   // Update Confirmation
+  async updateConfirmation(update, id) {
+    const confirmation = await Confirmation.findOne({where: {id}})
+    return await confirmation.update(update);
+  }; 
 
 
   // Delete Confirmation by ID
+  async deleteConfirmation(id) {
+    return Confirmation.destroy({where:{id}})
+  };
 }
 
 let confirmationController = new ConfirmationController();
--- routes/confirmation.router.js
+++ routes/confirmation.router.js
@@ -18,15 +18,59 @@ router.post('/', async (req,res) => {
 });
 
 // Get Confirmation by ID
+router.get ('/:id', async (req, res) => {
+  try {
 
+    const id = req.params.id;
+    const confirmation = await confirmationController.findConfirmation(id);
+    const confirmationByOwner = confirmation.confirmationByOwner
+    const dog_Id = confirmation.dog_Id;
+    const candidate_Id = confirmation.candidate_Id;
+    const user_Id = confirmation.user_Id;
+
+    res.json({confirmationByOwner, dog_Id, candidate_Id, user_Id})
+
+  }catch (err) {
+    return res.status(500).json({
+      message: err.message
+    });
+  };
+});
 
 // Get All Confirmations
+router.get ('/', async (req,res) => {
+  try{
+    res.json(await confirmationController.indexAll());
+  }catch(error){
+    res.status(500).json({
+      error: 'error',
+      message: 'error'
+    });
+  };
+});
 
 
 // Update Confirmation
-
+router.put('/:id', async (req, res) => {
+  try{
+    const body = req.body;
+    res.json(await confirmationController.updateConfirmation(body, req.params.id));
+  }catch(err){
+    return res.status(500).json({
+      message: err.message
+    });
+  };
+});
 
 // Delete Confirmation by ID
-
+router.delete('/:id', async (req, res) => {
+  try{
+    res.json(await confirmationController.deleteConfirmation(req.params.id));
+  }catch(err){
+    res.status(500).json({
+      message: err.message
+    });
+  };
+});
 
 module.exports = router;
