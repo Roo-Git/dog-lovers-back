e85f4b2bc091907c8e4024703da48f6d00d11bea
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-11 14:24:23 +0200

    Trying to deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const router = require ('./router');
 const db = require ('./db');
 const app = express();
 const port = 3000;
+
 const cors = require('cors')
 
 
@@ -23,7 +24,7 @@ app.use(router)
 
 // Start server
 db.then(() => {
-  app.listen(port, () => {
+  app.listen(process.env.MYSQL_DATABASE || port, () => {
       console.log(`Server app listening at http://localhost:${port}`);   
   });
 }).catch((err)=>console.log(err.message));
